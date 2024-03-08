8b796cc31e9259f7160efd27ce3585dadbc2bc6a
antonioortegas <antonioortega@uma.es>
2024-03-08 03:19:29 +0100

    implement (i think correctly) methods 'crear' and 'destruir'

--- Practica1/gestion_memoria.c
+++ Practica1/gestion_memoria.c
@@ -8,19 +8,24 @@
  * Inicialmente, s�lo un nodo desde 0 a MAX
 */
 void crear(T_Manejador* manejador){
-    (*manejador) = (T_Manejador)malloc(sizeof(struct T_Nodo));
-    (*manejador)->inicio = 0;
-    (*manejador)->fin = MAX-1;
-    (*manejador)->sig = NULL;
-    printf("Creando memoria\n");
+    *manejador = (T_Manejador*)malloc(sizeof(struct T_Nodo));
+    (*manejador)->inicio=0;
+    (*manejador)->fin=MAX-1;
+    (*manejador)->sig=NULL;
 }
 
 /* Destruye la estructura utilizada (libera todos los nodos de la lista.
  * El par�metro manejador debe terminar apuntando a NULL
 */
-void destruir(T_Manejador* manejador){
-    printf("Liberando memoria\n");
+void destruir(T_Manejador* manejador) {
+    T_Manejador aux = NULL;
+    while(*manejador!=NULL){
+        aux = *manejador;
+        *manejador = (*manejador)->sig;
+        free(aux);
+    }
 }
+
 
 /* Devuelve en �dir� la direcci�n de memoria �simulada� (unsigned) donde comienza
  * el trozo de memoria continua de tama�o �tam� solicitada.
@@ -28,28 +33,21 @@ void destruir(T_Manejador* manejador){
  * suficiente, devolvera TRUE (1) en �ok�; FALSE (0) en otro caso.
  */
 void obtener(T_Manejador *manejador, unsigned tam, unsigned* dir, unsigned* ok){
-    printf("Obteniendo memoria\n");
+    
 }
+
 
 /* Muestra el estado actual de la memoria, bloques de memoria libre */
 void mostrar (T_Manejador manejador){
-    if(manejador == NULL){
-        printf("No hay memoria\n");
-        return;
-    }
-    T_Manejador aux = manejador;
-    do
-    {
-        printf("Inicio: %d, Fin: %d\n", aux->inicio, aux->fin);
-        aux = aux->sig;
-    }while (aux->sig != NULL);
-    printf("Mostrando memoria\n");
+    
 }
+
 
 /* Devuelve el trozo de memoria continua de tama�o �tam� y que
  * comienza en �dir�.
  * Se puede suponer que se trata de un trozo obtenido previamente.
  */
-void devolver(T_Manejador *manejador,unsigned tam,unsigned dir){
-    printf("Devolviendo memoria\n");
+void devolver(T_Manejador *manejador, unsigned tam, unsigned dir) {
+    
 }
+
