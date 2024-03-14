84c49d88e41df0284e1537edbac1fa44ac7c449b
antonioortegas <antonioortega@uma.es>
2024-03-14 14:05:25 +0100

    first functions to read and write the binary file

---
+++ practica2/Practica2B.c
@@ -0,0 +1,95 @@
+/*
+ ============================================================================
+ Name        : Practica2B.c
+ Author      : esc
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "arbolbb.h"
+
+/**
+ * Pide un n�mero "tam" al usuario, y
+ * crea un fichero binario para escritura con el nombre "nfichero"
+ * en que escribe "tam" numeros (unsigned int) aleatorios
+ * Se utiliza srand(time(NULL)) para establecer la semilla (de la libreria time.h)
+ * y rand()%100 para crear un n�mero aleatorio entre 0 y TAM.
+ */
+void creafichero(char* nfichero){
+	FILE* fd;
+	srand(time(NULL));
+	int TAM;
+	printf("Numero de elementos a generar: ");
+	scanf("%d", &TAM);
+	printf("%d", TAM);
+	fd = fopen(nfichero,"wb");
+	if (fd == NULL){
+		perror("Error creando ficheroBinario.dat");
+	}
+	int number;
+	for(int i=0; i<TAM; i++){
+		number = rand()%TAM;
+		fwrite(&number,sizeof(int),1,fd);
+	}
+	fclose(fd);
+}
+/**
+ * Muestra por pantalla la lista de n�meros (unsigned int) almacenada
+ * en el fichero binario "nfichero"
+ */
+void muestrafichero(char* nfichero){
+	FILE* fd;
+	if ((fd = fopen(nfichero,"rb"))==NULL){
+		perror("Error abriendo ficheroBinario.dat");
+	}
+	int valor, leidos;
+	while ((leidos = fread(&valor,sizeof(int),1,fd)) > 0){
+		printf("%d ", valor);
+	}
+	fclose(fd);
+}
+
+/**
+ * Guarda en el arbol "*miarbol" los n�meros almacenados en el fichero binario "nfichero"
+ */
+
+void cargaFichero(char* nfichero, T_Arbol* miarbol){
+
+}
+
+int main(void) {
+	//setvbuf(stdout,NULL,_IONBF,0);
+
+	char nfichero[50];
+	printf ("Introduce el nombre del fichero binario:\n");
+	fflush(stdout);
+	scanf ("%s",nfichero);
+	fflush(stdin);
+	creafichero(nfichero);
+	printf("\nAhora lo leemos y mostramos:\n");
+	muestrafichero(nfichero);
+	fflush(stdout);
+/* COMMENT TREE FUNCTIONS FOR NOW
+	printf ("\nAhora lo cargamos en el arbol\n");
+	T_Arbol miarbol;
+	Crear (&miarbol);
+	cargaFichero(nfichero,&miarbol);
+	printf ("\nY lo mostramos ordenado\n");
+	Mostrar(miarbol);
+	fflush(stdout);
+	printf("\nAhora lo guardamos ordenado\n");
+	FILE * fich;
+	fich = fopen (nfichero, "wb");
+	Salvar (miarbol, fich);
+	fclose (fich);
+	printf("\nY lo mostramos ordenado\n");
+	muestrafichero(nfichero);
+	Destruir (&miarbol);
+*/
+	return EXIT_SUCCESS;
+}
---
+++ practica2/arbolbb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "arbolbb.h"
+
+// Crea la estructura utilizada para gestionar la memoria disponible.
+void Crear(T_Arbol* arbol){
+    
+}
+
+// Destruye la estructura utilizada.
+void Destruir(T_Arbol* arbol){
+
+}
+
+// Inserta num en el arbol
+void Insertar(T_Arbol* arbol,unsigned num){
+
+}
+
+// Muestra el contenido del �rbol en InOrden
+void Mostrar(T_Arbol arbol){
+
+}
+
+// Guarda en disco el contenido del arbol
+void Salvar(T_Arbol arbol, FILE* fichero){
+
+}
